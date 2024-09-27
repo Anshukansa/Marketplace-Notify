882b5e019913a1ccdf76345972df168ca91e3258
Anshukansa <91750490+Anshukansa@users.noreply.github.com>
2024-09-27 18:31:45 +1000

    Update market.py

--- market.py
+++ market.py
@@ -76,6 +76,7 @@ async def check_marketplace(keywords, location):
             print(f"Error checking marketplace for keyword '{keyword}': {e}")
 
         finally:
+            await send_telegram_message(f"Round Finish")
             driver.quit()  # Close the browser
 
 
