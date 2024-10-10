9a952038f74c919b68f9a9636c6fe6ae6dbd146a
Anshukansa <91750490+Anshukansa@users.noreply.github.com>
2024-10-10 14:04:19 +1100

    Update market.py

--- market.py
+++ market.py
@@ -4,12 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from telegram import Bot
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler
 
 # Set up Telegram bot
 TOKEN = '7621738371:AAFzRuvAFroszoqE-ciZp8Eyg-km7GloMq4'
-CHAT_ID = '5399212579'
-bot = Bot(token=TOKEN)
 
 # Set up Selenium WebDriver
 def create_webdriver():
@@ -21,23 +19,21 @@ def create_webdriver():
     return webdriver.Chrome(service=service, options=chrome_options)
 
 # Command handler for /title
-def get_title(update, context: CallbackContext):
+async def get_title(update, context):
     driver = create_webdriver()
     driver.get('https://www.google.com')
     time.sleep(2)  # Wait for the page to load
     title = driver.title
     driver.quit()
     
-    update.message.reply_text(f'The title of the page is: {title}')
+    await update.message.reply_text(f'The title of the page is: {title}')
 
 def main():
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
+    application = ApplicationBuilder().token(TOKEN).build()
 
-    dispatcher.add_handler(CommandHandler("title", get_title))
+    application.add_handler(CommandHandler("title", get_title))
 
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
